c28fc97dc294cdee435e33c4db6363a437b8b79c
Dani <Dani@DESKTOP-VQATU0S>
2024-01-16 19:39:14 +0100

    segundo commit suicide

--- proyecto1/src/proyecto1/main.java
+++ proyecto1/src/proyecto1/main.java
@@ -2,4 +2,14 @@ package proyecto1;
 
 public class main {
 		
+	public static void main(String[] args) {
+		
+		
+		System.out.println("me cago en todo");
+		System.out.println("No me gusta nada");
+	}
+	
+	
+	
+	
 }
